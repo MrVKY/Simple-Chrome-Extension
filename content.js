bcaab440c55556b2f880433410c10597ca05c6be
MrVKY <vignesh101012@gmail.com>
2020-03-30 22:54:13 +0530

    'Sample'

---
+++ content.js
@@ -0,0 +1,7 @@
+console.log("You are here!");
+var email = document.getElementById('email');
+if(email!=undefined)
+    email.value = "vignesh101012@gmail.com";
+var pass = document.getElementById('pass');
+if(pass!=undefined)
+    pass.focus();
